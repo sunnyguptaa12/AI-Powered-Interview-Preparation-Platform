df50c680f1c09d2fa1dfe68833855ba1cce22ab5
sunnyguptaa1210 <sunny887733gupta@gmail.com>
2026-05-11 00:51:51 +0530

    fixed production api url

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-powered-interview-preparation-platform-9ozd.onrender.com",
     withCredentials: true
 })
 
