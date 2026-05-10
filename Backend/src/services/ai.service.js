f1bb032fdf53c000e1b46eee34e8281c9aba5ea6
sunnyguptaa1210 <sunny887733gupta@gmail.com>
2026-05-10 23:52:30 +0530

    fix production auth

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -98,7 +98,7 @@ INSTRUCTIONS:
         console.log('🚀 Starting interview report generation...')
         
         const response = await ai.models.generateContent({
-            model:"gemini-1.5-flash",
+            model:"gemini-3-flash-preview",
             contents: prompt
         })
 
@@ -196,7 +196,7 @@ Return ONLY a JSON object:
         console.log('📄 Generating resume PDF...')
         
         const response = await ai.models.generateContent({
-            model: "gemini-1.5-flash",
+            model: "gemini-3-flash-preview",
             contents: prompt
         })
 
