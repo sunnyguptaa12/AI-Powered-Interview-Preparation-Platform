2e0708081b6bd50b3be8d08fc6e994ef2783ecb9
sunnyguptaa1210 <sunny887733gupta@gmail.com>
2026-05-11 01:08:10 +0530

    updated cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,24 +6,18 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://ai-powered-interview-preparation-pl-six.vercel.app",
-        "https://ai-powered-interview-preparation-platform-bkqvn5oax.vercel.app"
-    ],
+    origin: true,
     credentials: true
-}));
+}))
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
 /* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
-
-
 
 module.exports = app
