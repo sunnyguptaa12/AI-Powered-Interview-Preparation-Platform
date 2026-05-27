3d5a11c8865c778b787fc261693dec164c802aee
sunnyguptaa1210 <sunny887733gupta@gmail.com>
2026-05-27 13:11:32 +0530

    fixed production api

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
+    baseURL: "https://ai-powered-interview-preparation-platform-so7d.onrender.com/api",
     withCredentials: true,
 })
 
