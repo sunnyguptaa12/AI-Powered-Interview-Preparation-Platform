d536c7201eb203e2dc9f3fea2d615e5241afd579
sunnyguptaa1210 <sunny887733gupta@gmail.com>
2026-06-01 22:50:11 +0530

    Fix duplicate api routes

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -3,57 +3,68 @@ import axios from "axios";
 const api = axios.create({
     baseURL: "https://ai-powered-interview-preparation-platform-so7d.onrender.com/api",
     withCredentials: true,
-})
-
+});
 
 /**
- * @description Service to generate interview report based on user self description, resume and job description.
+ * Generate interview report
  */
-export const generateInterviewReport = async ({ jobDescription, selfDescription, resumeFile }) => {
+export const generateInterviewReport = async ({
+    jobDescription,
+    selfDescription,
+    resumeFile
+}) => {
 
-    const formData = new FormData()
-    formData.append("jobDescription", jobDescription)
-    formData.append("selfDescription", selfDescription)
-    formData.append("resume", resumeFile)
+    const formData = new FormData();
 
-    const response = await api.post("/api/interview/", formData, {
+    formData.append("jobDescription", jobDescription);
+    formData.append("selfDescription", selfDescription);
+    formData.append("resume", resumeFile);
+
+    const response = await api.post("/interview/", formData, {
         headers: {
             "Content-Type": "multipart/form-data"
         }
-    })
+    });
 
-    return response.data
-
-}
-
+    return response.data;
+};
 
 /**
- * @description Service to get interview report by interviewId.
+ * Get interview report by interviewId
  */
 export const getInterviewReportById = async (interviewId) => {
-    const response = await api.get(`/api/interview/report/${interviewId}`)
 
-    return response.data
-}
+    const response = await api.get(
+        `/interview/report/${interviewId}`
+    );
 
+    return response.data;
+};
 
 /**
- * @description Service to get all interview reports of logged in user.
+ * Get all interview reports
  */
 export const getAllInterviewReports = async () => {
-    const response = await api.get("/api/interview/")
 
-    return response.data
-}
+    const response = await api.get("/interview/");
 
+    return response.data;
+};
 
 /**
- * @description Service to generate resume pdf based on user self description, resume content and job description.
+ * Generate resume PDF
  */
-export const generateResumePdf = async ({ interviewReportId }) => {
-    const response = await api.post(`/api/interview/resume/pdf/${interviewReportId}`, null, {
-        responseType: "blob"
-    })
+export const generateResumePdf = async ({
+    interviewReportId
+}) => {
 
-    return response.data
-}
+    const response = await api.post(
+        `/interview/resume/pdf/${interviewReportId}`,
+        null,
+        {
+            responseType: "blob"
+        }
+    );
+
+    return response.data;
+};
