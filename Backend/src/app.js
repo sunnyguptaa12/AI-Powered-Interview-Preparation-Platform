71190fe87ab14f37be7cd0e0d69db1cc7c7daf89
sunnyguptaa1210 <sunny887733gupta@gmail.com>
2026-06-01 20:27:39 +0530

    change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://ai-powered-interview-pl-alpha.vercel.app"
+        "https://ai-powered-interview-gmial2kol.vercel.app"
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
