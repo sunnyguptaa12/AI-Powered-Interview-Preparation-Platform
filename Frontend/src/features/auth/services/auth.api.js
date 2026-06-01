93030e5d4232f1211b2b6d7ee15ab5c483844169
sunnyguptaa1210 <sunny887733gupta@gmail.com>
2026-06-01 20:18:24 +0530

    Updated project

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,66 +1,45 @@
 import axios from "axios"
 
-
 const api = axios.create({
     baseURL: "https://ai-powered-interview-preparation-platform-so7d.onrender.com/api",
     withCredentials: true
 })
 
 export async function register({ username, email, password }) {
-
     try {
-        const response = await api.post('/api/auth/register', {
+        const response = await api.post('/auth/register', {
             username, email, password
         })
-
         return response.data
-
     } catch (err) {
-
         console.log(err)
-
     }
-
 }
 
 export async function login({ email, password }) {
-
     try {
-
-        const response = await api.post("/api/auth/login", {
+        const response = await api.post("/auth/login", {
             email, password
         })
-
         return response.data
-
     } catch (err) {
         console.log(err)
     }
-
 }
 
 export async function logout() {
     try {
-
-        const response = await api.get("/api/auth/logout")
-
+        const response = await api.get("/auth/logout")
         return response.data
-
     } catch (err) {
-
     }
 }
 
 export async function getMe() {
-
     try {
-
-        const response = await api.get("/api/auth/get-me")
-
+        const response = await api.get("/auth/get-me")
         return response.data
-
     } catch (err) {
         console.log(err)
     }
-
 }
