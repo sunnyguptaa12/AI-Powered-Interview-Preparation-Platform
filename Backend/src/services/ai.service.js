299e671cdf718783080be78ec592a6daa0bf1444
sunnyguptaa1210 <sunny887733gupta@gmail.com>
2026-06-08 17:30:50 +0530

    change gemini

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -12,7 +12,7 @@ async function callGeminiWithRetry(prompt, retries = 3) {
     for (let i = 0; i < retries; i++) {
         try {
             const response = await ai.models.generateContent({
-                model: "gemini-2.5-flash",
+                model: "gemini-1.5-flash",
                 contents: prompt
             })
             return response
